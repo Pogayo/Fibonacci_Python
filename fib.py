5eefeb9f0cf2d55eae6884fb62e96f3787bc5fa7
pogayo <pogayo17@alustudent.com>
2018-10-20 20:22:00 +0300

    Added the rest of the code to the program

--- fib.py
+++ fib.py
@@ -1,10 +1,22 @@
 
-def fib( ) 
+def fib(number) 
+   if number==0:
+       fib_list=[]
+   elif number==1:
+       fib_list = [0]
+   else:
+       fib_list = [0, 1]
+       x = 0
+       last_index = 1
+       while (x<number-2):
+        fib_list.append(fib_list[last_index]+fib_list[last_index-1])
+        last_index+=1
+        x+=1
 
 
 
 
- return mylist
+   return fib_list 
 
 
 print (fib(20))
